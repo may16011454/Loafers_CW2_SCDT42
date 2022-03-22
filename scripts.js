9bffc9f6a34a11b164eeca271844f2cf5da2a42c
Logan May <loganmay@Logans-MacBook-Air.local>
2022-03-22 09:50:24 +0000

    adding notes to js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
+/* creates a read-only reference to a value and returns first element that matches .btn-toggle */
 const btn = document.querySelector(".btn-toggle");
+/* performs a check to see if the documnet matches the media query wich lets the object send notifications to listeners when its query state changes e.g when the value goes to false */
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+/* togles between the class and element */
 
 const currentTheme = localStorage.getItem("theme");
 if (currentTheme == "dark") {
@@ -7,6 +11,9 @@ if (currentTheme == "dark") {
 } else if (currentTheme == "light") {
   document.body.classList.toggle("light-theme");
 }
+
+/* attaches an even handler to the button
+Looks for the class name light-theme and changes the element when clicked */
 
 btn.addEventListener("click", function () {
   if (prefersDarkScheme.matches) {
